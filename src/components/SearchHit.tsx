95c3d35a2a74ae8fc1d40265e4ad7d156ec6cdf7
Michael <michael@scannable.io>
2023-11-22 11:01:36 +1300

    fix images

--- src/components/SearchHit.tsx
+++ src/components/SearchHit.tsx
@@ -1,5 +1,6 @@
 import { Hit as AlgoliaHit } from 'instantsearch.js';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import { Highlight } from 'react-instantsearch';
 
 type HitProps = {
@@ -10,19 +11,35 @@ type HitProps = {
     manufacturer: string;
   }>;
 };
-
+const fallbackImage = '/no-image.png';
 export default function SearchHit({ hit }: HitProps) {
+  const [error, setError] = useState<React.SyntheticEvent<
+    HTMLImageElement,
+    Event
+  > | null>(null);
+
+  const imageUrl = error ? fallbackImage : hit.image;
+  console.log({
+    error,
+    imageUrl,
+    hit,
+  });
+
+  useEffect(() => {
+    setError(null);
+  }, [hit]);
+
   return (
     <div className="hit">
-      {hit.image && (
-        <Image
-          src={hit.image}
-          width={100}
-          height="100"
-          alt={hit.name}
-          className="hit-image"
-        />
-      )}
+      <Image
+        src={imageUrl || fallbackImage}
+        width={100}
+        height="100"
+        alt={hit.name}
+        className="hit-image"
+        onError={setError}
+      />
+
       <div className="hit-details">
         <Highlight attribute="manufacturer" hit={hit} />
         <Highlight hit={hit} attribute="name" className="hit-label" />
